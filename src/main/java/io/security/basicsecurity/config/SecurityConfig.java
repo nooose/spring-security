1a809ee282ee07149b36f4a9da4257e952b0ead6
nooose <hd15807@gmail.com>
2023-03-08 01:33:47 +0900

    feat: Form 로그인 인증필터 적용
    UsernamePasswordAuthenticationFilter 적용을 확인

--- src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package io.security.basicsecurity.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
@@ -14,7 +16,22 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .anyRequest().authenticated();
-        http.formLogin();
+        http.formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler((request, response, authentication) -> {
+                    log.info("authentication " + authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {
+                    log.error("exception " + exception.getMessage());
+                    response.sendRedirect("/login");
+                })
+                .permitAll();
 
         return http.build();
     }
