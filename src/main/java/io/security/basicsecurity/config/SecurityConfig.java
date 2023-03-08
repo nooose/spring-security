b6190dfdd61d8eaf66512077c2c3606caca76ac7
nooose <hd15807@gmail.com>
2023-03-09 00:10:08 +0900

    feat: remember-me 필터 적용
    추가로 Lambda DSL 적용하면서 indent 문제를 해결

--- src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -1,33 +1,41 @@
 package io.security.basicsecurity.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Slf4j
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .anyRequest().authenticated();
-        http.formLogin();
-        http.logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler((request, response, authentication) -> {
-                    request.getSession().invalidate();
-                })
-                .logoutSuccessHandler((request, response, authentication) -> {
-                    response.sendRedirect("/login");
-                })
-                .deleteCookies("remember-me");
-
-        return http.build();
+        return http.authorizeHttpRequests(auth ->
+                        auth.anyRequest()
+                                .authenticated())
+                .formLogin(withDefaults())
+                .logout(logout ->
+                        logout.logoutUrl("/logout")
+                                .logoutSuccessUrl("/login")
+                                .addLogoutHandler((request, response, authentication) ->
+                                        request.getSession().invalidate())
+                                .logoutSuccessHandler((request, response, authentication) ->
+                                        response.sendRedirect("/login")))
+                .rememberMe(remember ->
+                        remember.rememberMeParameter("remember")
+                                .tokenValiditySeconds(3600)
+                                .userDetailsService(userDetailsService))
+                .build();
     }
 }
