754c306027939b554c509cf7772fe74a0c4ac66a
nooose <hd15807@gmail.com>
2023-03-18 19:56:10 +0900

    feat: SecurityContextHolder 확인

--- src/main/java/io/security/basicsecurity/controller/SecurityController.java
+++ src/main/java/io/security/basicsecurity/controller/SecurityController.java
@@ -1,13 +1,21 @@
 package io.security.basicsecurity.controller;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        log.info("{}", context.getAuthentication() == SecurityContextHolder.getContext().getAuthentication());
         return "home";
     }
 
